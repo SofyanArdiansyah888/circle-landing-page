eb14cbc0e93d5e9b6c7cb5d10396a01ee2192307
sofyan ardiansyah <sofyanardianysah@gmail.com>
2023-08-09 06:24:39 +0800

    add assets & fonts

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,3 @@
 import "./src/styles/global.css"
+import "@fontsource/ibm-plex-sans"
+import "@fontsource-variable/inter"
---
+++ src/components/SectionHero.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+const SectionHero = () => {
+    return <></>
+}
+
+export default SectionHero;
